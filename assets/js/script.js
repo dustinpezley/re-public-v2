9ad5b8bb7db1e9717114ca6c1c2ddc3290d1b5e1
dustinpezley <dustinpezley@gmail.com>
2022-06-19 10:23:00 -0500

    added open secrets API

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,20 +4,13 @@
 var address = '63119'
 
 const civicKey = 'AIzaSyABhC00ndBtZ48N0AC1XfQG911F09gmit8';
-// const proPublicaKey = '2pNm5c6OoX6Qs8joxqGptlpExvwrg9hxzGxzj3GE';
-
-// var cors_proxy = require('cors-anywhere');
-// cors_proxy.createServer({
-//     originWhitelist: [], // Allow all origins
-//     requireHeader: [],
-//     removeHeaders: []
-// });
-
-
+const proPublicaKey = '2pNm5c6OoX6Qs8joxqGptlpExvwrg9hxzGxzj3GE';
+const openSecretsKey = '790149a888a39934e82a2ad7234b7043';
 
 function getRepresentatives() {
   let apiUrl = "https://www.googleapis.com/civicinfo/v2/representatives?address="+address+"&key="+civicKey;
 
+  // redirects to proxy server for CORS workaround
   jQuery.ajaxPrefilter(function(apiUrl) {
     if (apiUrl.crossDomain && jQuery.support.cors) {
         apiUrl = 'https://whispering-bastion-84455.herokuapp.com/' + apiUrl;
@@ -33,43 +26,16 @@ function getRepresentatives() {
   })
 }
 
-getRepresentatives();
-
-// gapi.load("client");
-
-// function loadClient() {
-//   gapi.client.setApiKey("AIzaSyABhC00ndBtZ48N0AC1XfQG911F09gmit8");
-//   gapi.client.init({'clientId':'527860627972-ehld45ijhia5a8a1snjr2u72ha2m0mah.apps.googleusercontent.com'})
-//   return gapi.client.load("https://civicinfo.googleapis.com/$discovery/rest?version=v2")
-//       .then(function() { console.log("GAPI client loaded for API"); },
-//             function(err) { console.error("Error loading GAPI client for API", err); });
-// }
-// // Make sure the client is loaded before calling this method.
-// function execute() {
-//   return gapi.client.civicinfo.representatives.representativeInfoByAddress({
-//     "address": "63119",
-//     "includeOffices": false,
-//     "levels": [
-//       "regional"
-//     ]
-//   })
-//       .then(function(response) {
-//               // Handle the results here (response.result has the parsed body).
-//               console.log("Response", response);
-//             },
-//             function(err) { console.error("Execute error", err); });
-// }
-
-
-// loadClient();
-// execute();
-
 // function getCongressionalInfo() {
-//   var apiUrl = 'https://api.propublica.org/congress/v1/117';
+//   let apiUrl = 'https://api.propublica.org/congress/v1/';
 
-//   fetch(apiUrl,{
-//     headers: {"Authorization":"X-API-Key: 2pNm5c6OoX6Qs8joxqGptlpExvwrg9hxzGxzj3GE"}
-//   }).then(function(response){
+//   jQuery.ajaxPrefilter(function(apiUrl) {
+//     if (apiUrl.crossDomain && jQuery.support.cors) {
+//         apiUrl = 'https://whispering-bastion-84455.herokuapp.com/' + apiUrl;
+//     }
+//   });
+
+//   fetch(apiUrl).then(function(response){
 //     if(response.ok) {
 //       response.json().then(function(data){
 //         console.log(data);
@@ -79,3 +45,17 @@ getRepresentatives();
 // }
 
 // getCongressionalInfo();
+
+function getMemPFDProfile () {
+  let apiUrl = 'http://www.opensecrets.org/api/?method=memPFDprofile&year=2016&cid=N00007360&output=json&apikey='+openSecretsKey;
+
+  fetch(apiUrl).then(function(response) {
+    if(response.ok) {
+      response.json().then(function(data){
+        console.log(data);
+      })
+    }
+  })
+}
+
+getMemPFDProfile();
